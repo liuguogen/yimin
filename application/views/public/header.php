9bd04d5d585464b16916f65e583904bb47af07ab
liuguogen <630989951@qq.com>
2019-02-28 13:37:47 +0800

    链接

--- application/views/public/header.php
+++ application/views/public/header.php
@@ -3,11 +3,11 @@
         <div class="menu">
             <ul class="clear">
                 <a href="/"><li>HOME</li></a>
-                <a href="/"><li>公司介绍</li></a>
-                <a href="/"><li>直营项目</li></a>
+                <a href="<?php echo site_url('info/company_info') ?>"><li>公司介绍</li></a>
+                <a href="<?php echo site_url('info/direct_project') ?>"><li>直营项目</li></a>
                 <li style="margin-top: 10px;"><img class="logo" src="../../../assets/images/logo1.png"></li>
-                <a href="/"><li>战略项目</li></a>
-                <a href="/"><li>私人定制</li></a>
+                <a href="<?php echo site_url('info/strategy_project') ?>"><li>战略项目</li></a>
+                <a href="<?php echo site_url('info/tail_custom') ?>"><li>私人定制</li></a>
                 <a href="<?php echo site_url('info/about_group') ?>"><li>CONTACT</li></a>
             </ul>
         
