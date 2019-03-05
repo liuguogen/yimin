a8c9c7225cf06ad5e0e1b234427a797cb8b400a0
liuguogen <630989951@qq.com>
2019-03-05 21:37:50 +0800

    tp

--- application/views/public/header.php
+++ application/views/public/header.php
@@ -1,3 +1,111 @@
+
+<style type="text/css">
+body{
+   margin: 0;
+   padding: 0;
+   
+}
+.top{
+    background-image: url(../../../<?php echo $this->config->item('top_bg','home') ?>);
+   
+    height: 453px;
+    background-size: 100% 100%;    
+    background-repeat: no-repeat;
+    width: 100%;
+    background-position: center;
+}
+.clear{
+    clear: both;
+}
+ul,ol{
+    list-style: none;
+}
+.left{
+    float: left;
+}
+.right{
+    float: right;
+}
+.box{
+    width: 100%;
+    margin: 0 auto;
+
+}
+.menu {
+    padding: 0;
+    margin: 0;
+}
+.m{
+    width: 960px;
+    margin:0 auto;
+}
+
+.menu ul{
+    margin: 0;
+    padding: 0;
+}
+.menu ul li{
+    color: white;
+    float: left;
+    margin:60px 30px;
+    font-family: "微软雅黑"
+}
+.logo{
+    width: 120px;
+    height: 100px;
+    vertical-align:middle;
+}
+.text-info{
+    height: 40px;
+    width: 200px;
+    border: 2px solid white;
+    margin:15% 40%;
+    display: inline-table;
+}
+.text-info span{
+    color: white;
+    line-height: 40px;
+    text-align: center;
+    display: block;
+}
+.en-text{
+    margin:0 auto;
+    text-align: center;
+    font-size: 18px;
+    color: rgb(208,208,208);
+    margin-top: 50px;
+    font-family: "宋体";
+}
+.en-e{
+    margin:0 auto;
+    text-align: center;
+    font-size: 25px;
+    color: #000;
+    font-weight: bold;
+    margin-top: 20px;
+    font-family: "宋体";
+
+}
+
+.input-t{
+    margin-left: 80px;
+}
+.input-t input{
+    border: 2px solid #ccc;
+    height: 30px;
+    width: 240px;
+    margin: 0 0px 0 140px;
+}
+
+.input-t button{
+    height: 36px;
+    width: 100px;
+    color: white;
+    background: rgb(26,41,68);
+
+}
+
+</style>
 <div class="top box">
     <div class="m">
         <div class="menu">
